debf79719d2e8c8b1729d5e2ed914ab09ab83f94
Sol Zilberman <solzilbermancode@gmail.com>
2020-04-29 02:01:52 -0500

    button

---
+++ src/components/Options.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Fab } from '@material-ui/core'
+
+export default class Options extends Component {
+    render() {
+        return (
+            <div>
+                <Fab color="primary">Hello</Fab>
+            </div>
+        )
+    }
+}
