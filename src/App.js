ed249295c089bb6d7cefd0789d7be066ab9fcaf3
Sol Zilberman <solzilbermancode@gmail.com>
2020-04-29 02:05:07 -0500

    button update 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Options from "./components/Options"
 
 function App() {
   return (
     <div className="App">
       <h1>Code Deploy Test 2</h1>
+      <Options />
     </div>
   );
 }
